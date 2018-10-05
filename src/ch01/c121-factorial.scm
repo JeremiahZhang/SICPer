eef5643cda84828f4f1e73350b65d98ca52532b3
jeremyzhang <zhangleisuda@gmail.com>
2018-10-05 12:24:55 +0800

    factorial n

---
+++ src/ch01/c121-factorial.scm
@@ -0,0 +1,14 @@
+;;;;Factorial Example in ch01.02
+(define (factorial n)
+    (if (= n 1)
+        1
+        (* n (factorial (- n 1)))))
+
+(define (factorial-two n)
+    (define (fact-iter product counter n)
+        (if (> counter n)
+            product
+            (fact-iter (* product counter) 
+                       (+ 1 counter) 
+                       n)))
+    (fact-iter 1 1 n))
