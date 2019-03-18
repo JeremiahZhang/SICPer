16d1c294cbd32fba7de4d592579db54ba257d547
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-19 00:12:07 +0800

    rename

---
+++ src/ch01/ex112-pascal.scm
@@ -0,0 +1,20 @@
+;;;;Exercise 1.12 in ch01.02
+(define (pascal n)
+    (if (or (= n (accumulator (which—row 1 n))) 
+            (= n (+ 1 (accumulator (- (which—row 1 n) 1)))))
+        1
+        (+ (pascal (- n (which—row 1 n))) 
+           (pascal (+ 1 (- n (which—row 1 n)))))))
+
+
+(define (which—row counter a)
+    (if (and (< (accumulator counter) a) 
+             (< a (accumulator (+ counter 1))))
+        counter
+        (which—row (+ counter 1) a)))
+
+(define (accumulator n)
+    (if (= n 1)
+        1
+        (+ n (accumulator (- n 1)))))
+
