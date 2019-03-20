418d51b3afd5d45a447d2d8a2b03545f876cf8ae
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-20 22:52:46 +0800

    ex1.16

--- src/ch01/c124-expt.scm
+++ src/ch01/c124-expt.scm
@@ -25,9 +25,39 @@
 ; b^n = (b^(n/2))^2; if n is even
 ; b^n = b * b^(n-1); if n is odd
 (define (fast-expt b n)
-    (cond ((= n 1) 1)
+    (cond ((= n 0) 1)
           ((even? n) (square (fast-expt b (/ n 2))))
           (else (* b (fast-expt b (- n 1))))))
 
 (define (even? n)
     (= (remainder n 2) 0))
+
+;;;; ex1.16
+; Design a procedure that evolves an iterative
+; exponentiation process that uses successive 
+; squaring and use a logarithmic number of steps,
+; as does fast-expt.
+; Hint: b^n = (b^(n/2))^2 = (b^2)^(n/2)
+; b^1 = b
+; b^2 = b*b
+; b^3 = (b^2)^(3/2-1)*(b^2)
+; b^4 = (b^2)^(4/2)
+; n = 1, 3, 5, 7, 9排除
+; b^n = (b^2)^(n/2) , if n/2 is even, n为4的倍数
+; b^n = (b^2)^(n/2-1)*(b^2), if n/2 is odd, n非4倍的整数
+
+(define (o-expt b n)
+    (cond ((= n 0) 1)
+          ((= n 1) b)
+          ((even? (/ n 2)) 
+            (fast-expt (square b) (/ n 2)))
+          ((odd? (/n 2))
+            (* (square b) (fast-expt (square b)
+                (- (/ n 2) 1))))
+          (else xxx)
+        )
+
+(define (square b) (* b b))
+
+(defien (odd? n)
+    (not (= (remainder n 2) 0)))
