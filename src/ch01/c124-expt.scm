ec1ea3b9fe50512294c914f6378edcd486e2a2a7
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-20 21:14:57 +0800

    fast-expt

--- src/ch01/c124-expt.scm
+++ src/ch01/c124-expt.scm
@@ -18,3 +18,16 @@
         product
         (exptIter b (- counter 1)
                 (* b product))))
+
+; b^2 = b * b
+; b^4 = b**2 * b**2
+; b^8 = b^4 * b^4
+; b^n = (b^(n/2))^2; if n is even
+; b^n = b * b^(n-1); if n is odd
+(define (fast-expt b n)
+    (cond ((= n 1) 1)
+          ((even? n) (square (fast-expt b (/ n 2))))
+          (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+    (= (remainder n 2) 0))
