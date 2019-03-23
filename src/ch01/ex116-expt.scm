4cf04b76fc8088455b5bbc8e6d3112bcf80f6abe
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-23 20:20:20 +0800

    ex1.16

---
+++ src/ch01/ex116-expt.scm
@@ -0,0 +1,13 @@
+;;;;ex1.16
+
+(define (expt-exe b n)
+    (anoexpt b n 1))
+
+(define (anoexpt b n a)
+    (cond ((= n 0) a)
+          ((even? n) (anoexpt (square b) (/ n 2) a))
+          (else (anoexpt b (- n 1) (* a b))))
+    )
+
+(define (even? n)
+    (= (remainder n 2) 0))
