bc418b86e1bfffc507e878a2b4dafbaac884f1f2
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-24 15:19:19 +0800

    ex119

---
+++ src/ch01/ex119-fib.scm
@@ -0,0 +1,20 @@
+;ex119-fib.scm
+(define (fib n)
+    (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q counter)
+    (cond ((= counter 0) b)
+          ((even? counter)
+           (fib-iter a
+                     b
+                     (+ (square p) (square q)) ; p'
+                     (+ (* 2 p q) (square q))  ; q'
+                     (/ counter 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- counter 1))))) 
+
+(define (even? n)
+    (= (remainder n 2) 0))
